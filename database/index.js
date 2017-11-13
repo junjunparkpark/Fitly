8a909e52b7f71e3a2170eaa95c6b11ce27981fc8
Jun Park <jun.park@live.com>
2017-11-12 16:13:05 -0800

    Wrote initial structure for possible database NoSQL schema

---
+++ database/index.js
@@ -0,0 +1,52 @@
+
+// Possible Database Schema Designs
+
+// var userDay = {
+//   date: Date.now(),
+//     breakfast: {
+//     calories: 0,
+//       carbs: 0,
+//         fats: 0,
+//           protein: 0,
+//             sodium: 0,
+//               sugar: 0,
+//                 foods: []
+//   },
+//   lunch: {
+//     calories: 0,
+//       carbs: 0,
+//         fats: 0,
+//           protein: 0,
+//             sodium: 0,
+//               sugar: 0,
+//                 foods: []
+//   },
+//   dinner: {
+//     calories: 0,
+//       carbs: 0,
+//         fats: 0,
+//           protein: 0,
+//             sodium: 0,
+//               sugar: 0,
+//                 foods: []
+//   },
+//   snacks: {
+//     calories: 0,
+//       carbs: 0,
+//         fats: 0,
+//           protein: 0,
+//             sodium: 0,
+//               sugar: 0,
+//                 foods: []
+//   },
+//   exercise: {
+//     caloriesBurned: 0,
+//       exercises: []
+//   },
+//   notes: {
+//     text: ''
+//   },
+//   water: {
+//     total: 0
+//   }
+// }
